65a5665e3e13cac548a3516503db4b0a1ac040ca
Shital Shingare <sshingae3@gmail.com>
2026-04-01 16:18:54 +0530

    changes in loginpage

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,23 +1,51 @@
 import re
 from pages.base_page import BasePage
-from config.config import USERNAME, PASSWORD
-
+from config.config import USERNAME, PASSWORD,INVALID_USERNAME,INVALID_PASSWORD
+ 
 class LoginPage(BasePage):
-
+ 
     def __init__(self, page):
         super().__init__(page)
-
+ 
         # ✅ Enable locators
         self.username = page.get_by_placeholder("Your Email Address")
         self.password = page.get_by_placeholder("Password")
         self.login_btn = page.get_by_role("button", name=re.compile(r"Sign in", re.IGNORECASE))
-
+        self.errormsg = page.get_by_text("Minimum 6 characters required.")
+        self.wrongusername = page.locator("//div[@class='cdk-overlay-container']")
+        self.emptyusername = page.locator("//mat-error[@id='mat-mdc-error-1']")
+ 
     # 🔹 Open Login Page
     def load(self, url):
         self.navigate_to(url)   # ✅ FIX
-
-    # 🔹 Perform Login
+ 
+    # 🔹New Method 1: Perform Login
     def login(self, username=USERNAME, password=PASSWORD):
         self.username.fill(username)
         self.password.fill(password)
         self.login_btn.click()
+ 
+# 🔹 New Method 2: Invalid Login Method
+    def login_with_invalid_credentials(self, username=INVALID_USERNAME, password=INVALID_PASSWORD):
+            self.username.fill(username)
+            self.password.fill(password)
+            self.login_btn.click()
+    # 🔹 Wait for error message
+            self.errormsg.wait_for(state="visible", timeout=5000)
+            return self.errormsg.text_content()
+ 
+#  🔹 New Method 3: Login with username
+    def login_with_usernameonly(self, username = USERNAME):
+        self.username.fill(username)
+        self.login_btn.click()
+        # 🔹 Wait for error message
+        self.wrongusername.wait_for(state="visible", timeout=5000)
+        return self.wrongusername.text_content()
+   
+    #  🔹 New Method 4: Login with password
+    def login_with_passwordameonly(self, password = PASSWORD):
+        self.password.fill(password)
+        self.login_btn.click()
+        # 🔹 Wait for error message
+        self.wrongusername.wait_for(state="visible", timeout=5000)
+        return self.wrongusername.text_content()
--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -1,15 +1,39 @@
 from pages.login_page import LoginPage
-from config.config import USERNAME, PASSWORD, DASHBOARD_URL, BASE_URL
+from config.config import USERNAME, PASSWORD, DASHBOARD_URL, BASE_URL,INVALID_PASSWORD,INVALID_USERNAME
 from playwright.sync_api import expect
-
+ 
+# 🔹 Test 1: Valid Login
 def test_login(page):
     login_page = LoginPage(page)
-
     login_page.load(BASE_URL)
-    print(BASE_URL)
-    
+    print(BASE_URL)  
     login_page.login(USERNAME, PASSWORD)
-
     page.wait_for_load_state("networkidle")
-
-    expect(page).to_have_url(DASHBOARD_URL)   # ✅ FIX
+    expect(page).to_have_url(DASHBOARD_URL)   # ✅ FIX    
+   
+def test_invalid_login(page):
+    login_page = LoginPage(page)
+    login_page.load(BASE_URL)
+    # 🔹 Call method
+    error_msg = login_page.login_with_invalid_credentials(INVALID_USERNAME,INVALID_PASSWORD)
+    print("Error Message:", error_msg)
+    # 🔹 Validate error message
+    assert "Minimum 6 characters required" in error_msg
+ 
+def test_username(page):
+    login_page = LoginPage(page)
+    login_page.load(BASE_URL)
+    # 🔹 Call method
+    error_msg = login_page.login_with_usernameonly(USERNAME)
+    print("Error Message:", error_msg)
+    # 🔹 Validate error message
+    assert " " in error_msg
+ 
+def test_password(page):
+    login_page = LoginPage(page)
+    login_page.load(BASE_URL)
+    # 🔹 Call method
+    error_msg = login_page.login_with_passwordameonly(PASSWORD)
+    print("Error Message:", error_msg)
+    # 🔹 Validate error message
+    assert " " in error_msg
